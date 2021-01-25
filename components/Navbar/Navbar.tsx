5e937cc7838639b13f4cfdf97028ae8f38cd3856
Igor Santos <igr.exe@gmail.com>
2021-01-25 10:34:18 -0300

    chore: update navbar mobile

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Button, Flex, Link, Stack, Box } from "@chakra-ui/react";
 import { fallDown as Menu } from "react-burger-menu";
-import { MobileView, BrowserView, isMobile } from "react-device-detect";
+import { isMobile } from "react-device-detect";
 import Brand from "./Brand";
 import { withTranslation } from "../../i18n";
 
@@ -11,21 +11,28 @@ export const LinkStyled = ({ children, ...props }) => (
   </Link>
 );
 
-const NavbarComponent = ({ children }) => {
+const NavbarComponent = ({ children, t }) => {
   const [open, setOpen] = useState(false);
+
+  let flexStyles = { padding: '0 60px' };
+  if (isMobile) {
+    flexStyles = { padding: '0 30px' };
+  }
 
   return (
     <Flex
       bg="nossas.blue"
       height="70px"
-      paddingX={isMobile ? "30px" : "60px"}
       alignItems="center"
       justifyContent="space-between"
+      {...flexStyles}
     >
       {isMobile ? (
         <>
-          <Brand />
-          <Button size="md">Doar</Button>
+          <Flex alignItems='center' justifyContent='space-between'>
+            <Brand />
+            <Button size="md">Doar</Button>
+          </Flex>
           <Menu
             pageWrapId="page-wrap"
             outerContainerId="page-container"
@@ -33,7 +40,47 @@ const NavbarComponent = ({ children }) => {
             onOpen={() => setOpen(true)}
             onClose={() => setOpen(false)}
           >
-            {children}
+            <Flex alignItems='center' justifyContent='space-between' ml='40px' mt='-22px'>
+              <Brand />
+              <Button size="md">Doar</Button>
+            </Flex>
+            <Flex className='bm-item-list-menu' flex='1' direction='column'>
+              {children}
+            </Flex>
+            <Flex alignItems='center'>
+              <Link href='#' flex={1}>
+                <img
+                  src="/static/media/i18n/en.svg"
+                  alt={t("i18n.button.en")}
+                />
+              </Link>
+              <Stack direction='row' spacing={3}>
+                <Link href='#'>
+                  <img
+                    src="/static/media/ig-icon.svg"
+                    alt={t("social.icons.instagram")}
+                  />
+                </Link>
+                <Link href='#'>
+                  <img
+                    src="/static/media/fb-icon.svg"
+                    alt={t("social.icons.facebook")}
+                  />
+                </Link>
+                <Link href='#'>
+                  <img
+                    src="/static/media/tt-icon.svg"
+                    alt={t("social.icons.twitter")}
+                  />
+                </Link>
+                <Link href='#'>
+                  <img
+                    src="/static/media/linkedin-icon.svg"
+                    alt={t("social.icons.linkedin")}
+                  />
+                </Link>
+              </Stack>
+            </Flex>
           </Menu>
         </>
       ) : (
@@ -51,7 +98,7 @@ const NavbarComponent = ({ children }) => {
 
 const Navbar = ({ t }) => {
   return (
-    <NavbarComponent>
+    <NavbarComponent t={t}>
       <LinkStyled href="#">{t("about")}</LinkStyled>
       <LinkStyled href="#">{t("projects")}</LinkStyled>
       <LinkStyled href="#">{t("work")}</LinkStyled>
