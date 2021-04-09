fa5a42c93f692d606f71e37fed0f87789d8b5d9a
Igor Santos <igr.exe@gmail.com>
2021-04-09 16:45:07 -0300

    feat: redirect I18nButton to en.nossas.org

--- components/I18nButton.tsx
+++ components/I18nButton.tsx
@@ -1,34 +1,25 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import NextI18n from '../i18n';
+import React from "react";
+import styled from "@emotion/styled";
+import NextI18n from "../i18n";
 
 const { withTranslation, i18n } = NextI18n;
 
 const ButtonStyled = styled.button`
   border: none;
   outline: none;
-`
+`;
 
 const I18nButton = ({ t }) => {
   const changeLanguage = () => {
-    i18n.changeLanguage(i18n.language === "pt-BR" ? "en" : "pt-BR");
+    // i18n.changeLanguage(i18n.language === "pt-BR" ? "en" : "pt-BR");
+    window.open("https://www.en.nossas.org/", "_self");
   };
 
   return (
     <ButtonStyled onClick={changeLanguage}>
-      {i18n.language === 'pt-BR' ? (
-        <img
-          src="/static/media/i18n/en.svg"
-          alt={t("i18n.button.en")}
-        />
-      ) : (
-        <img
-          src="/static/media/i18n/pt-BR.svg"
-          alt={t("i18n.button.pt-BR")}
-        />
-      )}
+      <img src="/static/media/i18n/en.svg" alt={t("i18n.button.en")} />
     </ButtonStyled>
   );
-}
+};
 
-export default withTranslation('common')(I18nButton);
+export default withTranslation("common")(I18nButton);
