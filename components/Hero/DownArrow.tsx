76ecd6678fb97edf73d5a45c38a4e64d0f6f63bc
Gloria Maria <gloriateodoro1@gmail.com>
2021-05-19 00:29:23 -0300

    feat: add arrow down on the covers of all pages

---
+++ components/Hero/DownArrow.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function DownArrow() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="21"
+      fill="none"
+      viewBox="0 0 14 21"
+    >
+      <path
+        fill="#fff"
+        fillRule="evenodd"
+        d="M.223 11.893a.715.715 0 011.077 0l6.096 6.682a.893.893 0 010 1.18.715.715 0 01-1.077 0L.223 13.075a.893.893 0 010-1.181z"
+        clipRule="evenodd"
+      ></path>
+      <path
+        fill="#fff"
+        fillRule="evenodd"
+        d="M13.491 11.893a.715.715 0 00-1.077 0L6.32 18.575a.893.893 0 000 1.18.715.715 0 001.077 0l6.095-6.681a.893.893 0 000-1.181z"
+        clipRule="evenodd"
+      ></path>
+      <path
+        fill="#fff"
+        fillRule="evenodd"
+        d="M6.857 19.035a.799.799 0 01-.797-.8V2.8c0-.442.357-.801.797-.801.44 0 .797.359.797.8v15.435c0 .442-.357.8-.797.8z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
+
+export default DownArrow;
