f1509fbf306d259c36661172059f4f8314253660
Igor Santos <igr.exe@gmail.com>
2021-06-01 21:53:52 -0300

    feat: add translate to trainings page

--- pages/trainings/index.tsx
+++ pages/trainings/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { NextPage } from "next";
 import { WithUserAgentProps, withUserAgent } from "next-useragent";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import {
   Heading,
   Text,
@@ -17,7 +18,7 @@ import Hero from "../../components/Hero";
 import { SliderPanel, Carousel, ImageText } from "../../components/Slider";
 
 const Trainings: React.FC<WithUserAgentProps> = ({ ua }) => {
-  const t = (i18nKey: string, _?: any) => i18nKey;
+  const { t } = useTranslation("trainings");
 
   return (
     <Body>
@@ -25,8 +26,7 @@ const Trainings: React.FC<WithUserAgentProps> = ({ ua }) => {
       <Hero
         maxWidth="1440px"
         videoUrl="/static/media/covers/trainings.mp4"
-        // title={t("content:covers.training.title")}
-        title="Receba treinamento em metodologias de ativismo"
+        title={t("cover")}
         titleColor="white"
         titleAlign="center"
       />
@@ -34,71 +34,55 @@ const Trainings: React.FC<WithUserAgentProps> = ({ ua }) => {
       <Section>
         <Header
           title={
-            <>
-              Vem <b>aprender!</b>
-            </>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: t("learning.title", {
+                  interpolation: { escapeValue: false },
+                }),
+              }}
+            />
           }
-          description={`
-            Temos um programa de treinamento e mentoria para ativistas iniciantes
-            e experientes do Brasil inteiro! Confira nossa agenda para não perder
-            sua inscrição.
-          `}
+          description={t("learning.description")}
         />
       </Section>
 
       <Section direction={["column"]} spacing={["30px"]}>
         <Heading as="h2" color="pink.main">
-          {/* <div
+          <div
             dangerouslySetInnerHTML={{
-              __html: t("content:blocks.training.doing.title", {
+              __html: t("now.title", {
                 interpolation: { escapeValue: false },
               }),
             }}
-          /> */}
-          Rolando <b>agora</b>
+          />
         </Heading>
         <SliderPanel
-          isMobile={ua.isMobile}
+          isMobile={ua?.isMobile || false}
           infiniteLoop
           items={[
             {
               src: "/static/media/s3/muvuca.png",
               href: "https://www.muvuca.nossas.org/",
-              // alt: t("content:blocks.training.doing.carousel.1.title"),
-              // title: t("content:blocks.training.doing.carousel.1.title"),
-              // description: t("content:blocks.training.doing.carousel.1.description"),
-              // link: t("content:blocks.training.doing.carousel.1.action"),
-              alt: "MUVUCA",
-              title: "MUVUCA",
-              description:
-                "O primeiro programa de ativismo climático do NOSSAS vai oferecer treinamento ao longo de seis meses para jovens que lutam por um futuro viável para nosso planeta.",
-              link: "Saiba mais",
+              alt: t("now.carousel.1.title"),
+              title: t("now.carousel.1.title"),
+              description: t("now.carousel.1.description"),
+              link: t("now.carousel.1.action"),
             },
             {
               src: "/static/media/s3/programademobilizadores.png",
               href: "https://mobilizadores.nossas.org/",
-              // alt: t("content:blocks.training.doing.carousel.2.title"),
-              // title: t("content:blocks.training.doing.carousel.2.title"),
-              // description: t("content:blocks.training.doing.carousel.2.description"),
-              // link: t("content:blocks.training.doing.carousel.2.action"),
-              alt: "PROGRAMA DE MOBILIZADORES",
-              title: "PROGRAMA DE MOBILIZADORES",
-              description:
-                "As turmas deste programa recebem gratuitamente treinamento em mobilizações, comunicação de campanhas e tecnologias de ativismo.",
-              link: "Inscreva-se para a próxima turma",
+              alt: t("now.carousel.2.title"),
+              title: t("now.carousel.2.title"),
+              description: t("now.carousel.2.description"),
+              link: t("now.carousel.2.action"),
             },
             {
               src: "/static/media/s3/jovens-mobilizadoras.png",
               href: "https://www.programademobilizadoras.nossas.org/",
-              // alt: t("content:blocks.training.doing.carousel.3.title"),
-              // title: t("content:blocks.training.doing.carousel.3.title"),
-              // description: t("content:blocks.training.doing.carousel.3.description"),
-              // link: t("content:blocks.training.doing.carousel.3.action"),
-              alt: "PROGRAMA DE JOVENS MOBILIZADORAS",
-              title: "PROGRAMA DE JOVENS MOBILIZADORAS",
-              description:
-                "O programa é voltado para jovens estudantes do Ensino Médio. Esta edição realizada em parceria com a Girl Up! está treinando 100 jovens meninas no tema pobreza menstrual.",
-              link: "Saiba mais",
+              alt: t("now.carousel.3.title"),
+              title: t("now.carousel.3.title"),
+              description: t("now.carousel.3.description"),
+              link: t("now.carousel.3.action"),
             },
           ]}
         />
@@ -106,59 +90,47 @@ const Trainings: React.FC<WithUserAgentProps> = ({ ua }) => {
 
       <Section direction={["column"]} spacing="30px" bgColor="gray.light">
         <Heading as="h2" color="blue.main">
-          {/* <div
+          <div
             dangerouslySetInnerHTML={{
-              __html: t("content:blocks.training.victories.title", {
+              __html: t("victories.title", {
                 interpolation: { escapeValue: false },
               }),
             }}
-          /> */}
-          Vitórias dos <b>mobilizadores</b>
+          />
         </Heading>
         <SliderPanel
           infiniteLoop
-          isMobile={ua.isMobile}
+          isMobile={ua?.isMobile || false}
           items={[
             {
               src: "/static/media/s3/boonline.png",
               href: "https://www.boonlineparaelas.bonde.org/",
-              // alt: t("content:blocks.training.victories.carousel.1.title"),
-              // title: t("content:blocks.training.victories.carousel.1.title"),
-              // description: t("content:blocks.training.victories.carousel.1.description"),
-              // link: t("content:blocks.training.victories.carousel.action"),
-              alt: "B.O Online para elas - Minas Gerais",
-              title: "B.O Online para elas - Minas Gerais",
-              description:
-                "O Coletivo Abertha conseguiu implementar a Delegacia Virtual do Estado de Minas Gerais depois de uma campanha que recebeu nossa mentoria. Agora, mulheres podem fazer seus boletins de ocorrência sem sair de casa.",
-              link: "Saiba mais",
+              alt: t("victories.carousel.1.title"),
+              title: t("victories.carousel.1.title"),
+              description: t("victories.carousel.1.description"),
+              link: t("victories.carousel.1.action"),
             },
             {
               src: "/static/media/s3/revitalizacao.png",
               href: "https://revitalizafarol.bonde.org",
-              // alt: t("content:blocks.training.victories.carousel.2.title"),
-              // title: t("content:blocks.training.victories.carousel.2.title"),
-              // description: t("content:blocks.training.victories.carousel.2.description"),
-              // link: t("content:blocks.training.victories.carousel.action"),
-              alt: "Revitalização do Farol - Maranhão",
-              title: "Revitalização do Farol - Maranhão",
-              description:
-                "Os ex-estudantes do Centro São José Operário, em São Luiz, participaram de nossa primeira turma e conseguiram através de uma campanha de pressão, a revitalização do Farol, onde acontecem atividades educacionais para a comunidade.",
-              link: "Saiba mais",
+              alt: t("victories.carousel.2.title"),
+              title: t("victories.carousel.2.title"),
+              description: t("victories.carousel.2.description"),
+              link: t("victories.carousel.2.action"),
             },
           ]}
         />
       </Section>
 
       <Section direction="column" spacing="30px">
-        <Heading as="h2" color="pink.main" size="lg">
-          {/* <div
+        <Heading as="h2" color="pink.main" size="lg" fontWeight="bold">
+          <div
             dangerouslySetInnerHTML={{
-              __html: t("content:blocks.incubations.projects.title", {
+              __html: t("testimonials.title", {
                 interpolation: { escapeValue: false },
               }),
             }}
-          /> */}
-          <b>Depoimentos</b>
+          />
         </Heading>
         <Carousel
           infiniteLoop
@@ -166,27 +138,22 @@ const Trainings: React.FC<WithUserAgentProps> = ({ ua }) => {
           items={[
             <ImageText
               src="/static/media/s3/depoimento.png"
-              description={`"Ai, gente! Muito lindo e muito potente participar disso e ver tanta gente junto fazendo acontecer. Muito histórico."`}
-              userName="Ana Clara - Programa de Mobilizadores Região Amazônica (Jul. 2020)"
+              description={t("testimonials.carousel.1.description")}
+              userName={t("testimonials.carousel.1.people")}
             />,
             <ImageText
               src="/static/media/s3/depoimento2.png"
-              description={
-                "Queria agradecer ao NOSSAS. Vocês são maravilhosas, o treinamento é incrível, a metodologia, é tudo muito organizado! Vocês arrasam demais! Parabéns!!"
-              }
-              userName="Bruna B. - Programa de Jovens Mobilizadoras (Nov 2020)"
+              description={t("testimonials.carousel.2.description")}
+              userName={t("testimonials.carousel.2.people")}
             />,
             <ImageText
               src="/static/media/s3/depoimento3.png"
-              description={
-                "Gente, eu to me arrepiando a cada módulo. É tão potente nosso poder de transformação coletiva. Tô sentindo uma conexão tão potente, uma vibração tão forte. Estou muito feliz e grata por esta oportunidade de formação"
-              }
-              userName="Bruna M. - Programa de Mobilizadores Região Amazônica (Jul. 2020)"
+              description={t("testimonials.carousel.3.description")}
+              userName={t("testimonials.carousel.3.people")}
             />,
           ]}
         />
       </Section>
-
       <Section>
         <SimpleGrid
           columns={[1, null, null, 2]}
@@ -198,20 +165,15 @@ const Trainings: React.FC<WithUserAgentProps> = ({ ua }) => {
           </Box>
           <Stack textAlign={["center", null, null, "left"]} spacing={["20px"]}>
             <Heading as="h2" color="green">
-              {/* <div
+              <div
                 dangerouslySetInnerHTML={{
-                  __html: t("content:blocks.training.program.title", {
+                  __html: t("participate.title", {
                     interpolation: { escapeValue: false },
                   }),
                 }}
-              /> */}
-              Participe do <b>programa de treinamento</b>
+              />
             </Heading>
-            <Text>
-              {/* {t("content:blocks.training.program.description")} */}
-              Temos treinamentos temáticos acontecendo durante o ano inteiro.
-              Clique para saber os cronogramas e temas para 2021.
-            </Text>
+            <Text>{t("participate.description")}</Text>
             <Button
               maxW="190px"
               marginX={["auto !important", null, null, "0 !important"]}
@@ -220,8 +182,7 @@ const Trainings: React.FC<WithUserAgentProps> = ({ ua }) => {
                 href="https://www.mobilizadores.nossas.org/#block-35353"
                 target="blank"
               >
-                {/* {t("content:blocks.training.program.button")} */}
-                Quero saber
+                {t("participate.action")}
               </a>
             </Button>
           </Stack>
@@ -231,8 +192,12 @@ const Trainings: React.FC<WithUserAgentProps> = ({ ua }) => {
   );
 };
 
-// Trainings.getInitialProps = async () => ({
-//   namespacesRequired: ["common", "content"],
-// });
+export const getStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "trainings"])),
+    },
+  };
+};
 
 export default withUserAgent(Trainings);
