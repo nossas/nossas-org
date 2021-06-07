68b0a0a83cc08b16a2bc41f691bc4729c9f38977
Gloria Teodoro <gloriateodoro1@gmail.com>
2021-06-07 17:51:09 -0300

    refactor: refactor ImageText component

---
+++ pages/incubations/ImageTextIncubations.tsx
@@ -0,0 +1,47 @@
+import { Heading, Stack, Text, Img, Link } from "@chakra-ui/react";
+
+interface ImageTextIncubationsProps {
+  src: string;
+  alt?: string;
+  title: string;
+  description: string;
+  href?: string;
+  linkText?: string;
+}
+
+export const ImageTextIncubations: React.FC<ImageTextIncubationsProps> = ({
+  alt,
+  src,
+  title,
+  description,
+  href,
+  linkText,
+}) => {
+  return (
+    <Stack
+      direction={["column", "column", null, null, "row"]}
+      spacing="70px"
+      alignItems="center"
+    >
+      <Img
+        objectFit="cover"
+        boxSize={["100%", null, null, null, "357px"]}
+        src={src}
+        alt={alt}
+      />
+      <Stack spacing="10px" textAlign="left">
+        <Link href={href} target="_blank">
+          <Heading as="h3" color="blue.main" fontWeight="bold" size="md">
+            {title}
+          </Heading>
+        </Link>
+        <Text>{description}</Text>
+        {href && linkText ? (
+          <Link href={href} target="_blank" variant="pink">
+            {`${linkText} +`}
+          </Link>
+        ) : null}
+      </Stack>
+    </Stack>
+  );
+};
