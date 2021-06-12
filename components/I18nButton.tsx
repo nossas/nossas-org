ae57897e2d8697b0daa3c381d74cdf03989f4491
Lucas Pirola <lucaspirola@gmail.com>
2021-06-12 14:04:01 -0300

    Revert "fix: change url to en pages"
    This reverts commit 2f8314b751ba3e9f25cd11d985a604285b83ee9b.

--- components/I18nButton.tsx
+++ components/I18nButton.tsx
@@ -1,33 +1,22 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
-// import { useRouter } from "next/router";
-// import Link from "next/link";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 const I18nButton: React.FC<{ extraProps?: { [key: string]: any } }> = ({
   extraProps,
 }) => {
-  // const { locale, pathname } = useRouter();
-  // const toggleLocale = locale === "pt-BR" ? "en" : "pt-BR";
-  // const labelLocale = locale === "pt-BR" ? "EN" : "BR";
+  const { locale, pathname } = useRouter();
+  const toggleLocale = locale === "pt-BR" ? "en" : "pt-BR";
+  const labelLocale = locale === "pt-BR" ? "EN" : "BR";
 
   return (
-    <Button
-      variant="menu"
-      onClick={() => {
-        window.open("https://www.en.nossas.org", "_self");
-      }}
-      {...(extraProps || {})}
-    >
-      EN
-    </Button>
+    <Link href={pathname} locale={toggleLocale}>
+      <Button variant="menu" {...(extraProps || {})}>
+        {labelLocale}
+      </Button>
+    </Link>
   );
-  // return (
-  //   <Link href={pathname} locale={toggleLocale}>
-  //     <Button variant="menu" {...(extraProps || {})}>
-  //       {labelLocale}
-  //     </Button>
-  //   </Link>
-  // );
 };
 
 export default I18nButton;
