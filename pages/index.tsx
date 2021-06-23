99e11fdc542948610b5d509c3978211c81ad7a15
Gloria Teodoro <gloriateodoro1@gmail.com>
2021-06-23 11:45:24 -0300

    fix: fix donation section on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,12 @@
-import { Flex, Heading, Image, Text, Stack } from "@chakra-ui/react";
+import {
+  Flex,
+  SimpleGrid,
+  Box,
+  Heading,
+  Image,
+  Text,
+  Stack,
+} from "@chakra-ui/react";
 import { WithUserAgentProps, withUserAgent } from "next-useragent";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
@@ -140,38 +148,39 @@ const Home: React.FC<WithUserAgentProps> = ({ ua }) => {
         direction={["column", null, null, "row"]}
         spacing={["30px"]}
       >
-        <Image
-          maxW={[226, 339]}
-          maxH={[232, 347]}
-          margin="0 auto"
-          src="/static/media/s3/sustentabilidade.png"
-          alt="Sustentabilidade"
-        />
-
-        <Stack
-          maxW="710px"
-          margin="0 auto"
-          flex={1.5}
-          spacing={8}
-          alignItems={["center", null, null, "flex-start"]}
-        >
-          <Heading
-            as="h2"
-            size="lg"
-            color="green"
-            dangerouslySetInnerHTML={{
-              __html: t("sustainability.donate", {
-                interpolation: { escapeValue: false },
-              }),
-            }}
+        <SimpleGrid columns={[1, null, null, 2]}>
+          <Image
+            maxW={[226, 339]}
+            maxH={[232, 347]}
+            margin="0 auto"
+            src="/static/media/s3/sustentabilidade.png"
+            alt="Sustentabilidade"
           />
 
-          <Text textAlign={["center", "left"]}>
-            {t("sustainability.description")}
-          </Text>
+          <Stack
+            flex={1.5}
+            spacing={8}
+            alignItems={["center", null, null, "flex-start"]}
+          >
+            <Heading
+              as="h2"
+              size="lg"
+              color="green"
+              mt={7}
+              dangerouslySetInnerHTML={{
+                __html: t("sustainability.donate", {
+                  interpolation: { escapeValue: false },
+                }),
+              }}
+            />
 
-          <Donation>{t("sustainability.navigate")}</Donation>
-        </Stack>
+            <Text textAlign={["center", "left"]}>
+              {t("sustainability.description")}
+            </Text>
+
+            <Donation>{t("sustainability.navigate")}</Donation>
+          </Stack>
+        </SimpleGrid>
       </Section>
 
       <ContentMedia
