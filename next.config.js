81b03faa26002ede5622fc4c3c1e77e3123922a9
Lucas Pirola <lucaspirola@gmail.com>
2021-12-01 21:18:44 -0300

    feat: add bundle size config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-// Internalization Routing
+// next.config.js
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 const { i18n } = require("./next-i18next.config");
 
-module.exports = {
-  i18n
+module.exports = (phase, defaultConfig) => {
+  return withBundleAnalyzer({...defaultConfig, i18n})
 }
