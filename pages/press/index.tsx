3b27a4d100b8f1bc6ac51cf73193cf19afcea836
Gloria Teodoro <gloriateodoro1@gmail.com>
2022-01-28 15:24:11 -0300

    feat: add translation to press page

--- pages/press/index.tsx
+++ pages/press/index.tsx
@@ -4,7 +4,7 @@ import { WithUserAgentProps } from "next-useragent";
 import { Body, Section } from "../../components/Page";
 import Hero from "../../components/Hero";
 import { Header } from "../../content";
-import { Text, Link, Stack, VStack } from "@chakra-ui/react";
+import { Text, Link, Stack } from "@chakra-ui/react";
 import SocialMedia from "./SocialMedia";
 import { useTranslation } from "next-i18next";
 
@@ -25,8 +25,6 @@ const Press: React.FC<WithUserAgentProps> = () => {
       />
 
       <Section>
-        {/* -------------------- */}
-        {/* // Nesse trecho a tradução funciona */}
         <Header
           title={
             <div
@@ -38,42 +36,54 @@ const Press: React.FC<WithUserAgentProps> = () => {
             />
           }
           description={
-            <VStack spacing={7} maxW={725}>
-              {/* -------------------- */}
-              {/* Já esse trecho não está sendo traduzido */}
+            <Stack spacing={7} maxW={725}>
               <Text>
                 <div
                   dangerouslySetInnerHTML={{
-                    __html: t("journalist.title", {
+                    __html: t("journalist.p1", {
                       interpolation: { escapeValue: false },
                     }),
                   }}
                 />
               </Text>
               <Text>
-                Nos mobilizamos por inúmeras causas tanto de alcance nacional,
-                quanto no âmbito local, por meio da Rede Nossas Cidades, do
-                Norte ao Sul do Brasil.
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: t("journalist.p2", {
+                      interpolation: { escapeValue: false },
+                    }),
+                  }}
+                />
               </Text>
               <Text>
-                Foram 15 campanhas vitoriosas em 2021 que mobilizaram milhares
-                de pessoas, voluntários e organizações parceiras em todo o país,
-                e cujo impacto transformou positivamente a vida de milhares de
-                brasileiros.
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: t("journalist.p3", {
+                      interpolation: { escapeValue: false },
+                    }),
+                  }}
+                />
               </Text>
               <Text>
-                Justamente por isso, somos uma fonte quase inesgotável de
-                grandes histórias e personagens; desde aqueles que colocam a mão
-                na massa para fazer do mundo um lugar mais solidário e
-                democrático, àqueles cujas vidas são para sempre transformadas
-                pelo nosso trabalho.
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: t("journalist.p4", {
+                      interpolation: { escapeValue: false },
+                    }),
+                  }}
+                />
               </Text>
               <Text>
-                Se você deseja saber mais sobre o que estamos fazendo agora ou
-                sobre alguma das nossas campanhas ativas, escreva para a gente
-                em {nossasEmail}.
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: t("journalist.p5", {
+                      interpolation: { escapeValue: false },
+                    }),
+                  }}
+                />{" "}
+                {nossasEmail}
               </Text>
-            </VStack>
+            </Stack>
           }
         />
       </Section>
@@ -81,23 +91,12 @@ const Press: React.FC<WithUserAgentProps> = () => {
       <Section bgColor="gray.light">
         <Header
           titleColor="pink.main"
-          title={
-            <>
-              Sobre <b>o Nossas</b>
-            </>
-          }
+          title={t("journalist.about")}
           description={
             <>
               <Stack spacing={7}>
-                <Text>
-                  O NOSSAS impulsiona o ativismo solidário e democrático no
-                  Brasil. Temos como missão fortalecer os movimentos e pessoas
-                  que estejam se organizando para fazer a diferença no país, e
-                  fazemos isso compartilhando metodologias, dando treinamentos e
-                  oferecendo uma plataforma online de petições e campanhas, o
-                  BONDE.
-                </Text>
-                <Text>Acompanhe o nosso trabalho pelos nossos canais:</Text>
+                <Text>{t("journalist.description")}</Text>
+                <Text>{t("journalist.description2")}</Text>
                 <SocialMedia />
               </Stack>
             </>
@@ -111,7 +110,7 @@ const Press: React.FC<WithUserAgentProps> = () => {
 export const getStaticProps = async ({ locale }) => {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["common", "trainings"])),
+      ...(await serverSideTranslations(locale, ["common", "press"])),
     },
   };
 };
