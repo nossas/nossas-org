6597307d84d2f530113f19f088214c9aef57834a
Gloria Teodoro <gloriateodoro1@gmail.com>
2023-04-09 19:03:48 -0300

    feat: add email icon

--- components/SocialMedia/index.tsx
+++ components/SocialMedia/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Heading, Img, Link, Stack } from "@chakra-ui/react";
+import { Heading, Link, Stack } from "@chakra-ui/react";
 import { useTranslation } from "next-i18next";
-import { Facebook, Instagram, Twitter, Linkedin, Youtube } from "./IconsSVG";
+import {
+  Facebook,
+  Instagram,
+  Twitter,
+  Linkedin,
+  Youtube,
+  Email,
+} from "./IconsSVG";
 
 interface SocialMediaProps {
   color?: string;
@@ -71,12 +78,7 @@ const SocialMedia: React.FC<SocialMediaProps> = ({ color, flat }) => {
       </Stack>
       {!flat && (
         <Stack>
-          <Img
-            mt={2}
-            maxWidth="200px"
-            src="static/media/footer/email.png"
-            alt="Email do Nossas"
-          />
+          <Email />
         </Stack>
       )}
     </Stack>
