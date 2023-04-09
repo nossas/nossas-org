6d074a4cb430605550f73c5e60dfbcec183ffadc
Gloria Teodoro <gloriateodoro1@gmail.com>
2023-04-09 20:23:24 -0300

    refactor: update footer alignment components

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Grid, UnorderedList, ListItem, Link } from "@chakra-ui/react";
+import { Box, Stack, UnorderedList, ListItem, Link } from "@chakra-ui/react";
 import { useTranslation } from "next-i18next";
 import NextLink from "next/link";
 
@@ -14,57 +14,59 @@ const Footer = () => {
 
   return (
     <Section as="footer">
-      <Grid
-        templateRows={["repeat(3, auto)", "auto"]}
-        templateColumns={["auto", "auto", "repeat(3, auto)"]}
-        rowGap={["60px"]}
-        columnGap={[0, "90px"]}
+      <Box>
+        <NextLink href="/">
+          <FooterLogo />
+        </NextLink>
+      </Box>
+      <Stack
+        direction={["column", "column", "row"]}
         alignItems="baseline"
+        justifyContent="space-between"
+        spacing={[50, 100]}
       >
-        <Box textTransform="uppercase">
-          <NextLink href="/">
-            <FooterLogo />
-          </NextLink>
+        <UnorderedList
+          styleType="none"
+          ml={0}
+          mt={[5, 20]}
+          textTransform="uppercase"
+        >
+          <ListItem>
+            <NextLink href="/about">
+              <Link>{t("footer.navigation.about")}</Link>
+            </NextLink>
+          </ListItem>
 
-          <UnorderedList styleType="none" ml={0} mt={[5, 20]}>
+          <ListItem>
+            <NextLink href="/#join-us">
+              <Link>{t("footer.navigation.join-us")}</Link>
+            </NextLink>
+          </ListItem>
+
+          <ListItem>
+            <NextLink href="/mobilizations">
+              <Link>{t("footer.navigation.mobilizations")}</Link>
+            </NextLink>
+          </ListItem>
+          <ListItem>
+            <Donation variant="link" colorScheme="blue">
+              {t("footer.navigation.donate")}
+            </Donation>
+          </ListItem>
+
+          {i18n.language !== "en" && (
             <ListItem>
-              <NextLink href="/about">
-                <Link>{t("footer.navigation.about")}</Link>
+              <NextLink href="/work-with-us">
+                <Link>{t("footer.navigation.work-with-us")}</Link>
               </NextLink>
             </ListItem>
-
-            <ListItem>
-              <NextLink href="/#join-us">
-                <Link>{t("footer.navigation.join-us")}</Link>
-              </NextLink>
-            </ListItem>
-
-            <ListItem>
-              <NextLink href="/mobilizations">
-                <Link>{t("footer.navigation.mobilizations")}</Link>
-              </NextLink>
-            </ListItem>
-
-            <ListItem>
-              <Donation variant="link" colorScheme="blue">
-                {t("footer.navigation.donate")}
-              </Donation>
-            </ListItem>
-
-            {i18n.language !== "en" && (
-              <ListItem>
-                <NextLink href="/work-with-us">
-                  <Link>{t("footer.navigation.work-with-us")}</Link>
-                </NextLink>
-              </ListItem>
-            )}
-          </UnorderedList>
-        </Box>
+          )}
+        </UnorderedList>
 
         <NewsletterForm />
 
         <SocialMedia />
-      </Grid>
+      </Stack>
     </Section>
   );
 };
