11f82cbdbfa6afe43ea28cc72daac9ee1d48922c
Gloria Teodoro <gloriateodoro1@gmail.com>
2023-08-30 12:54:59 -0300

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NextApp from "next/app";
 import { appWithTranslation } from "next-i18next";
 import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import ReactGA from "react-ga";
 
 import apm from "../lib/apm";
 // import NextI18n from "../i18n";
@@ -10,12 +11,20 @@ import { nossas } from "../theme";
 import { createApolloClient } from "../lib";
 // add global css
 import "../styles/globals.css";
+import { useRouter } from "next/router";
+
+ReactGA.initialize("G-7R11G6KBCH");
 
 const theme = extendTheme(nossas);
 
 apm.setInitialPageLoadName("Home NOSSAS");
-
 function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    ReactGA.pageview(router.pathname);
+  }, [router.pathname]);
+
   return (
     <ChakraProvider theme={theme}>
       <ApolloProvider client={createApolloClient({}, {}) as any}>
